4ff63ad6d835eac56df1c915f328b79efd87c809
manhhung012 <32127425+manhhung012@users.noreply.github.com>
2022-02-20 14:17:44 +0700

    upload

---
+++ src/main/java/com/example/storecase/StoreCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.storecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storecase/StoreCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
